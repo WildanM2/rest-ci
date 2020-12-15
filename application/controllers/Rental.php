68982e0e4b03ab27e6b9abb4e454d7805ade3d7f
Wildan Maulidi <wildancb08@gmail.com>
2020-12-15 08:13:28 +0700

    Rest CI

---
+++ application/controllers/Rental.php
@@ -0,0 +1,130 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+// Don't forget include/define REST_Controller path
+
+/**
+ *
+ * Controller Rental
+ *
+ * This controller for ...
+ *
+ * @package   CodeIgniter
+ * @category  Controller REST
+ * @author    Setiawan Jodi <jodisetiawan@fisip-untirta.ac.id>
+ * @author    Raul Guerrero <r.g.c@me.com>
+ * @link      https://github.com/setdjod/myci-extension/
+ * @param     ...
+ * @return    ...
+ *
+ */
+
+use chriskacerguis\RestServer\RestController;
+
+class Rental extends RestController
+{
+    
+  public function __construct()
+  {
+    parent::__construct();
+    $this->load->model('rental_model', 'rnt');
+  }
+
+  public function index_get()
+  {
+    $id = $this->get('id');
+    if ($id === null){
+      $p = $this->get('page');
+      $p = (empty($p) ?  1 : $p);
+      $total_data= $this->rnt->count();
+      $total_page = ceil($total_data / 5);
+      $start = ($p - 1) * 5;
+    $list = $this->rnt->get(null, 5, $start);
+    if ($list) {
+    $data = [
+      'status' => true,
+      'page' => $p,
+      'total_data' => $total_data,
+      'total_page' => $total_page,
+      'data' => $list
+    ];
+   } else {
+    $data=[
+      'status'=>false,
+      'msg'=> 'Data tidak ditemukan'
+    ];
+   }
+    $this->response($data, RestController::HTTP_OK);
+    } else {
+      $data = $this->rnt->get($id);
+      if ($data){
+      $this->response(['status' => true,'data' => $data], RestController::HTTP_OK);
+      } else{
+        $this->response(['status' => false,'msg' => 'Harga ' . $id . ' tidak ditemukan'], RestController::HTTP_NOT_FOUND);
+      }
+    }
+  }
+
+  public function index_post()
+  {
+    $data = [
+      'No_mobil'=>$this->post('No_mobil'),
+      'No_polisi'=>$this->post('No_polisi'),
+      'Nama_mobil'=>$this->post('Nama_mobil'),
+      'Warna'=>$this->post('Warna'),
+      'Harga_sewa'=>$this->post('Harga_sewa')
+    ];
+    $simpan=$this->rnt->add($data);
+    if($simpan['status']){
+      $this->response(['status'=>true,'msg'=>$simpan['data']. ' Data telah ditambahkan'], RestController::HTTP_CREATED);
+    } else {
+      $this->response(['status'=>false, 'msg'=>$simpan['msg']], RestController::HTTP_INTERNAL_ERROR);
+    }
+  }
+
+  public function index_put()
+  {
+    $data = [
+      'No_mobil'=>$this->put('No_mobil'),
+      'No_polisi'=>$this->put('No_polisi'),
+      'Nama_mobil'=>$this->put('Nama_mobil'),
+      'Warna'=>$this->put('Warna'),
+      'Harga_sewa'=>$this->put('Harga_sewa')
+    ];
+    $id=$this->put('id');
+    if($id===null){
+      $this->response(['status'=>false, 'msg'=> 'Masukkan Nomer Mobil yang akan dirubah'], RestController::HTTP_BAD_REQUEST);
+    }
+    $simpan=$this->rnt->update($id, $data);
+    if($simpan['status']){
+      $status=(int)$simpan['data'];
+      if ($status>0)
+      $this->response(['status'=>true,'msg'=>$simpan['data']. ' Data telah dirubah'], RestController::HTTP_OK);
+      else
+      $this->response(['status'=>false, 'msg'=> 'Tidak ada data yang dirubah'], RestController::HTTP_BAD_REQUEST);
+    } else {
+      $this->response(['status'=>false, 'msg'=>$simpan['msg']], RestController::HTTP_INTERNAL_ERROR);
+    }
+  }
+
+  public function index_delete()
+  {
+    $id=$this->delete('id');
+    if($id===null){
+      $this->response(['status'=>false, 'msg'=> 'Masukkan Nomer Mobil yang akan dihapus'], RestController::HTTP_BAD_REQUEST);
+    }
+    $delete= $this->rnt->delete($id);
+    if($delete['status']) {
+      $status=(int)$delete['data'];
+      if ($status>0)
+      $this->response(['status'=>true,'msg'=> $id. ' data telah dihapus'], RestController::HTTP_OK);
+      else
+      $this->response(['status'=>false, 'msg'=> 'Tidak ada data yang dihapus'], RestController::HTTP_BAD_REQUEST);
+    } else {
+      $this->response(['status'=>false, 'msg'=>$delete['msg']], RestController::HTTP_INTERNAL_ERROR);
+    }
+  }
+}
+
+
+/* End of file Rental.php */
+/* Location: ./application/controllers/Rental.php */
---
+++ application/models/Rental_model.php
@@ -0,0 +1,90 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+/**
+ *
+ * Model Rental_model
+ *
+ * This Model for ...
+ * 
+ * @package		CodeIgniter
+ * @category	Model
+ * @author    Setiawan Jodi <jodisetiawan@fisip-untirta.ac.id>
+ * @link      https://github.com/setdjod/myci-extension/
+ * @param     ...
+ * @return    ...
+ *
+ */
+
+class Rental_model extends CI_Model {
+
+  // ------------------------------------------------------------------------
+
+  public function __construct()
+  {
+    parent::__construct();
+  }
+
+  public function get($id = null, $limit = 5, $offset = 0)
+  {
+    if ($id === null)
+    {
+     return $this->db->get('mobil', $limit, $offset)->result();
+    } else{
+      return $this->db->get_where('mobil', ['Harga_sewa' => $id])->result_array();
+    }
+  }
+
+  public function count()
+  {
+    return $this->db->get('mobil')->num_rows();
+  }
+
+  public function add($data)
+  {
+    try{
+      $this->db->insert('mobil',$data);
+      $error=$this->db->error();
+      if(!empty($error['code'])){
+        throw new Exception('Terjadi kesalahan: '.$error['message']);
+        return false;
+      }
+      return ['status'=>true,'data'=>$this->db->affected_rows()];
+    } catch(Exception $ex) {
+      return ['status'=>false,'msg'=>$ex->getMessage()];
+    }
+  }
+
+  public function update($id, $data)
+  {
+    try{
+      $this->db->update('mobil',$data,['No_mobil'=>$id]);
+      $error=$this->db->error();
+      if(!empty($error['code'])){
+        throw new Exception('Terjadi kesalahan: '.$error['message']);
+         return false;
+      }
+      return ['status'=>true,'data'=>$this->db->affected_rows()];
+    } catch(Exception $ex) {
+      return ['status'=>false,'msg'=>$ex->getMessage()];
+    }
+  }
+
+  public function delete($id)
+  {
+    try{
+      $this->db->delete('mobil', ['No_mobil' => $id]);
+      $error=$this->db->error();
+      if(!empty($error['code'])){
+        throw new Exception('Terjadi kesalahan: '. $error['message']);
+        return false;
+      }
+      return ['status'=>true,'data'=>$this->db->affected_rows()];
+    } catch(Exception $ex) {
+      return ['status'=>false,'msg'=>$ex->getMessage()];
+    }
+  }
+}
+
+/* End of file Rental_model.php */
+/* Location: ./application/models/Rental_model.php */
